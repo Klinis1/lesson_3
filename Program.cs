5ef4fb88c4ced31b76a98da202d1e48c3c435344
Klinis1 <egor.ptrk1@gmail.com>
2022-10-13 23:59:48 +0300

    change

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.WriteLine("Input data:");
+var data1= Console.ReadLine();
+var data2 = Console.ReadLine();
+var data3 = int.TryParse(data1, out int data);
+var data5 = int.TryParse(data2, out int data4);
+int b = 0;
+var Od = data4;
+var Do =data;
+if (data<=data4)
+{
+    Od=data;
+    Do=data4;
+}
+if (data3 && data5)
+{
+    for (int i =Od; i <= Do; i++)
+    {
+        b= b+i;
+    }
+    Console.WriteLine(b);
+}
+else
+    Console.WriteLine("Incorrect input");
